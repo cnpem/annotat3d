5bdbdc3a079b3f5b3fcba474e234709e3bfd50a5
Alan Peixinho <alanpeixinho81@gmail.com>
2022-03-16 08:24:55 -0300

    menu fab button component

---
+++ src/components/MenuFabButton.tsx
@@ -0,0 +1,42 @@
+import {Fragment, useState} from 'react';
+
+import { IonFab, IonFabList, IonFabButton, IonIcon} from '@ionic/react';
+
+interface ButtonProps {
+    id: string;
+    logo: string;
+}
+
+interface MenuButtonProps {
+    buttonsList: ButtonProps[];
+    onChange?: (button: ButtonProps) => void;
+};
+
+const MenuFabButton: React.FC<MenuButtonProps> = ({buttonsList, onChange}) => {
+
+    const [selected, setSelected] = useState(buttonsList[0]);
+
+    return (
+        <Fragment>
+            <IonFabButton color='dark'>
+                <IonIcon size="large" icon={selected.logo}/>
+            </IonFabButton>
+            <IonFabList side="top">
+                {buttonsList.map((item) => {
+                    return (<IonFabButton>
+                        <IonIcon key={item.id} icon={item.logo} onClick={() => {
+                            if (selected.id !== item.id) {
+                                setSelected(item);
+                                if (onChange) {
+                                    onChange(item);
+                                }
+                            }
+                        }}/>
+                    </IonFabButton>);
+                })}
+            </IonFabList>
+        </Fragment>
+    );
+}
+
+export default MenuFabButton;
--- src/pages/Page.tsx
+++ src/pages/Page.tsx
@@ -1,48 +1,47 @@
-import { IonButtons, IonContent, IonFooter, IonHeader, IonInput, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonContent, IonFooter, IonHeader, IonInput, IonMenuButton, IonPage, IonSplitPane, IonTitle, IonToolbar } from '@ionic/react';
 import { useParams } from 'react-router';
 import ExploreContainer from '../components/ExploreContainer';
 import './Page.css';
 
-import MenuFabButton from '../components/MenuFabButton'
 import CanvasContainer from '../components/CanvasContainer';
 import {useState} from 'react';
 
+
+
 const Page: React.FC = () => {
 
-  const { name } = useParams<{ name: string; }>();
+    const { name } = useParams<{ name: string; }>();
 
-  const [sliceXY, setSliceXY] = useState<number>(0)
+    const [sliceXY, setSliceXY] = useState<number>(0);
 
-  return (
-    <IonPage>
-      <IonHeader>
 
-        <IonToolbar color="primary">
-          <IonButtons slot="start">
-            <IonMenuButton/>
-          </IonButtons>
-          <IonTitle>{name}</IonTitle>
-        </IonToolbar>
-      </IonHeader>
+    return (
+        <IonPage>
+            <IonHeader>
 
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">{name}</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-          <IonInput type="number" value={sliceXY} onIonChange={(e) => { setSliceXY(+e.detail.value!) } } />
-          { /* <ExploreContainer name={name} /> */ }
-          <CanvasContainer z={sliceXY}/>
-          <MenuFabButton></MenuFabButton>
-      </IonContent>
+                <IonToolbar color="primary">
+                    <IonButtons slot="start">
+                        <IonMenuButton/>
+                    </IonButtons>
+                    <IonTitle>{name}</IonTitle>
+                </IonToolbar>
+            </IonHeader>
 
-    <IonFooter>
-        <span>{sliceXY}</span>
-    </IonFooter>
-
-    </IonPage>
-  );
+            <IonContent fullscreen>
+                <IonHeader collapse="condense">
+                    <IonToolbar>
+                        <IonTitle size="large">{name}</IonTitle>
+                    </IonToolbar>
+                </IonHeader>
+                <IonInput type="number" value={sliceXY} onIonChange={(e) => { setSliceXY(+e.detail.value!) } } />
+                { /* <ExploreContainer name={name} /> */ }
+                <CanvasContainer z={sliceXY}/>
+            </IonContent>
+            <IonFooter>
+                <span>{sliceXY}</span>
+            </IonFooter>
+        </IonPage>
+    );
 };
 
 export default Page;
