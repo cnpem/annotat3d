f092448b0b55e54e5a9c7abf1b252d874e5c4256
Alan Peixinho <alanpeixinho81@gmail.com>
2022-03-22 13:40:04 -0300

    test fixing sscIO

--- build_container.sh
+++ build_container.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t gccdockers/annotat3d:cuda-11.2 --build-arg GCC_PYPI_SERVER="$GCC_PYPI_SERVER" --build-arg GCC_PYPI_HOST="$GCC_PYPI_HOST" .
-
-echo "$GCC_PYPI_HOST"
-
-echo "$GCC_PYPI_SERVER"
+#docker build -t gccdockers/annotat3d:cuda-11.2 --build-arg GCC_PYPI_SERVER="$GCC_PYPI_SERVER" --build-arg GCC_PYPI_HOST="$GCC_PYPI_HOST" .
 
 singularity build Annotat3DWeb.sif Singularity
 
