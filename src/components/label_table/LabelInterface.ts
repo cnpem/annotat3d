d0a16ce2354b39f072ddf47a67080322f45f5983
Alan Peixinho <alanpeixinho81@gmail.com>
2022-03-23 15:02:41 -0300

    label_table folder

---
+++ src/components/label_table/LabelInterface.ts
@@ -0,0 +1,13 @@
+/**
+ * This script contains all the labels interfaces calls
+ */
+
+/**
+ * Child component for the ToolbarComp
+ */
+export interface LabelInterface{
+    id: number;
+    color: [number, number, number];
+    labelName: string;
+}
+
---
+++ src/components/label_table/LabelTable.tsx
@@ -0,0 +1,77 @@
+import React, {useState} from "react";
+import * as ReactBootStrap from "react-bootstrap";
+import {IonTitle, IonRow, IonCol} from "@ionic/react";
+import InputLabel from "./InputLabel";
+import OptionsIcons from "./OptionsIcons";
+import {LabelInterface} from './LabelInterface';
+
+import './LabelTable.css';
+
+/**
+ * Component that creates the label table
+ * @constructor
+ * @return this components returns the label table
+ */
+const LabelTable: React.FC = () => {
+
+    const BACKGROUND_COLOR: [number, number, number] = [229, 16, 249];// pink
+
+    const [newLabelId, setNewLabelId] = useState<number>(1);
+    const [labelList, setLabelList] = useState<LabelInterface[]>([{ labelName: "Background", color: BACKGROUND_COLOR, id: 0 }]);
+
+    const selectLabelList = (labels: LabelInterface[]) => {
+        setLabelList(labels);
+    }
+
+    const selectIdGenerator = (id: number) => {
+        setNewLabelId(id + 1);
+    }
+
+    const renderLabel = (labelElement: LabelInterface, index: number) => {
+
+        return(
+            <tr key={index}>
+                <td>
+                    <div style={ {display: "flex"} }>
+                        <div className="round-bar" style={{ background: `rgb(${labelElement.color.join(',')})` }}> </div>
+                        {labelElement.labelName}
+                    </div>
+                </td>
+                <td>
+                    <OptionsIcons labelList={labelList} onRemoveLabel={setLabelList}
+                        id={labelElement.id}/>
+                </td>
+            </tr>
+        );
+
+    };
+
+    const NAME_WIDTH = "col-3";
+    const OPTIONS_WIDTH = "col-1";
+
+    return(
+        <React.Fragment>
+            <IonTitle>Label card</IonTitle>
+            <IonRow>
+                <IonCol>
+                    <InputLabel labelList={labelList} onLabelList={selectLabelList}
+                        newLabelId={newLabelId} onNewLabelId={selectIdGenerator}/>
+                </IonCol>
+            </IonRow>
+            <ReactBootStrap.Table striped bordered hover>
+                <thead>
+                    <tr>
+                        <th className={NAME_WIDTH}>Label Name</th>
+                        <th className={OPTIONS_WIDTH}>Options</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {labelList.map(renderLabel)}
+                </tbody>
+            </ReactBootStrap.Table>
+        </React.Fragment>
+    );
+
+};
+
+export default LabelTable;
