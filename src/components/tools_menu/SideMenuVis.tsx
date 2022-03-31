d1a3d61cb929d760219680414c28f9a3a22c09a3
Alan Peixinho <alanpeixinho81@gmail.com>
2022-03-31 08:51:41 -0300

    fix initial value for dual knobs ionrange

--- src/components/tools_menu/SideMenuVis.tsx
+++ src/components/tools_menu/SideMenuVis.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {IonCard, IonCardContent, IonRange, IonIcon} from "@ionic/react";
 import {moon, sunny} from "ionicons/icons";
 import {dispatch} from "../../utils/eventbus";
@@ -9,12 +9,23 @@ const SideMenuVis: React.FC = () => {
         lower: 10,
         upper: 90
     });
+
+    const contrastRangeRef = useRef<HTMLIonRangeElement | null>(null);
+
+    //for some weird reason, IonRange is ignoring value when using the value property,
+    //so I am manually setting it.
+    useEffect(() => {
+        if (contrastRangeRef) {
+            contrastRangeRef.current!.value = contrast;
+        }
+    }, [contrast, contrastRangeRef]);
+
     return(
         <React.Fragment>
             <IonCard>
                 <IonCardContent>
-                    <IonRange pin={true} debounce={300}
-                        value={ {lower: contrast.lower, upper: contrast.upper} } dualKnobs={true} onIonChange={ (e) => {
+                    <IonRange ref={contrastRangeRef} pin={true} debounce={300}
+                        dualKnobs={true} onIonChange={ (e) => {
                             if (e.detail.value) {
                                 const range = e.detail.value as any;
                                 setContrast(range);
