0feb38c87cabb3674040faee3bf7b874e876caf3
Bruno Carlos <bruno.carlos@lnls.br>
2022-05-31 17:07:19 -0300

    Crop preview event working with canvas. Still needs to know axis and update on a slice change.

---
+++ src/components/tools_menu/CropShapeInterface.ts
@@ -0,0 +1,9 @@
+/**
+ * Interface component for CropShapeInterface
+ * 
+ * Carries the indexes of lower and upper bounds of a cropped image over an axis 
+ */
+export default interface CropShapeInterface{
+    lower: number;
+    upper: number;
+}
