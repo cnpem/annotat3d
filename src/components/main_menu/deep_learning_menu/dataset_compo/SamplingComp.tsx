25b524b03b53c11f2c0578104c58c509480e119f
gabriel.macedo <gabriel.macedo@lnls.br>
2022-06-08 15:45:00 -0300

    The first part of SamplingComp.tsx is done. Just need to create the rest (prob use Bootstrap)

--- src/components/main_menu/deep_learning_menu/dataset_compo/SamplingComp.tsx
+++ src/components/main_menu/deep_learning_menu/dataset_compo/SamplingComp.tsx
@@ -1,20 +1,34 @@
 import React from "react";
 import {
     IonAccordion,
-    IonAccordionGroup,
+    IonAccordionGroup, IonCol,
     IonContent,
-    IonIcon,
+    IonIcon, IonInput,
     IonItem,
     IonItemDivider,
     IonLabel,
-    IonList
+    IonList, IonRow
 } from "@ionic/react";
 import {construct} from "ionicons/icons";
+import {useStorageState} from "react-storage-hooks";
+
+interface SamplingInterface {
+    nClasses: number,
+    sampleSize: number,
+    patchSize: Array<number>;
+}
 
 /**
  * Component that hold all the Sampling options
  */
 const SamplingComp: React.FC = () => {
+
+    const [sampleElement, setSampleElement] = useStorageState<SamplingInterface>(sessionStorage, "sampleElement", {
+        nClasses: 2,
+        sampleSize: 100,
+        patchSize: [256, 256, 1],
+    });
+
     return (
         <small>
             <IonContent
@@ -72,8 +86,83 @@ const SamplingComp: React.FC = () => {
                             <IonLabel><small>Sampling</small></IonLabel>
                         </IonItem>
                         <IonList slot={"content"}>
+                            {/*# Classes*/}
                             <IonItem>
-                                <IonLabel>bla ?</IonLabel>
+                                <IonRow>
+                                    <IonCol>
+                                        <IonLabel># Classes</IonLabel>
+                                        <div style={{display: "flex", justifyContent: "flex-start"}}>
+                                            <IonInput
+                                                type={"number"}
+                                                min={"0"} value={sampleElement.nClasses}
+                                                onIonChange={(e: CustomEvent) => {
+                                                    setSampleElement({
+                                                        ...sampleElement,
+                                                        nClasses: parseInt(e.detail.value!, 10)
+                                                    })
+                                                }}/>
+                                        </div>
+                                    </IonCol>
+                                </IonRow>
+                            </IonItem>
+                            {/*Sample Size*/}
+                            <IonItem>
+                                <IonRow>
+                                    <IonCol>
+                                        <IonLabel>Sample Size</IonLabel>
+                                        <div style={{display: "flex", justifyContent: "flex-start"}}>
+                                            <IonInput
+                                                type={"number"}
+                                                min={"0"} value={sampleElement.sampleSize}
+                                                onIonChange={(e: CustomEvent) => {
+                                                    setSampleElement({
+                                                        ...sampleElement,
+                                                        sampleSize: parseInt(e.detail.value!, 10)
+                                                    })
+                                                }}/>
+                                        </div>
+                                    </IonCol>
+                                </IonRow>
+                            </IonItem>
+                            {/*Patch Size*/}
+                            <IonItem>
+                                <IonRow>
+                                    <IonCol>
+                                        <IonLabel>Patch Size</IonLabel>
+                                        <div style={{display: 'flex', justifyContent: 'flex-start'}}>
+                                            <IonInput
+                                                type="number"
+                                                min={"0"}
+                                                value={sampleElement.patchSize[0]}
+                                                placeholder="X"
+                                                onIonChange={(e: CustomEvent) => setSampleElement({
+                                                    ...sampleElement,
+                                                    patchSize: [parseInt(e.detail.value!, 10), sampleElement.patchSize[1], sampleElement.patchSize[2]]
+                                                })}
+                                            />
+                                            <IonInput
+                                                type="number"
+                                                min={"0"}
+                                                value={sampleElement.patchSize[1]}
+                                                placeholder="Y"
+                                                onIonChange={(e: CustomEvent) => setSampleElement({
+                                                    ...sampleElement,
+                                                    patchSize: [sampleElement.patchSize[0], parseInt(e.detail.value!, 10), sampleElement.patchSize[2]]
+                                                })}
+                                            />
+                                            <IonInput
+                                                type="number"
+                                                min={"0"}
+                                                value={sampleElement.patchSize[2]}
+                                                placeholder="Z"
+                                                onIonChange={(e: CustomEvent) => setSampleElement({
+                                                    ...sampleElement,
+                                                    patchSize: [sampleElement.patchSize[0], sampleElement.patchSize[1], parseInt(e.detail.value!, 10)]
+                                                })}
+                                            />
+                                        </div>
+                                    </IonCol>
+                                </IonRow>
                             </IonItem>
                             <IonItemDivider/>
                         </IonList>
