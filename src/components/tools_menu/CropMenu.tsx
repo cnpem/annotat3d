4d2d561f34893cc9c80693d92229ebdd2a742bec
bruno.carlos <bruno.carlos@lnls.br>
2022-06-22 17:12:57 -0300

    Solving sliders references on crop menu.

--- src/components/tools_menu/CropMenu.tsx
+++ src/components/tools_menu/CropMenu.tsx
@@ -1,5 +1,6 @@
 import { IonItem, IonLabel, IonRange } from '@ionic/react';
-import { Fragment } from 'react';
+import { isEqual } from 'lodash';
+import { Fragment, useEffect, useRef } from 'react';
 import { useStorageState } from 'react-storage-hooks';
 
 import { dispatch } from '../../utils/eventbus';
@@ -37,6 +38,35 @@ const CropMenu: React.FC<CropMenuProps> = (props: CropMenuProps) => {
         upper: props.imageShape.z
     });
 
+    // positional range references for sliders
+    const rangeRefX = useRef<HTMLIonRangeElement | null>(null);
+    const rangeRefY = useRef<HTMLIonRangeElement | null>(null);
+    const rangeRefZ = useRef<HTMLIonRangeElement | null>(null);
+
+    useEffect(() => {
+        if (rangeRefX) {
+            if (!isEqual(rangeRefX.current!.value, cropX)) {
+                setTimeout(() => {
+                    rangeRefX.current!.value = cropX;
+                }, 20);
+            }
+        }
+        if (rangeRefY) {
+            if (!isEqual(rangeRefY.current!.value, cropY)) {
+                setTimeout(() => {
+                    rangeRefY.current!.value = cropY;
+                }, 20);
+            }
+        }
+        if (rangeRefZ) {
+            if (!isEqual(rangeRefZ.current!.value, cropZ)) {
+                setTimeout(() => {
+                    rangeRefZ.current!.value = cropZ;
+                }, 20);
+            }
+        }
+    });
+
     function onPreview() {
         const cropShape:CropShapeInterface = {
             cropX: cropX,
@@ -60,15 +90,7 @@ const CropMenu: React.FC<CropMenuProps> = (props: CropMenuProps) => {
         sfetch("POST", "/crop_image/image", JSON.stringify(cropShape), "json")
         .then((img_info:ImageInfoInterface) => {
 
-            // const info: ImageInfoInterface = {
-            //     imageShape: img_info["image_shape"],
-            //     imageDtype: img_info["image_dtype"],
-            //     imageName: img_info["image_name"],
-            //     imageExt: img_info["image_ext"],
-            // }
-            // setShowErrorWindow(false);
             dispatch('ImageLoaded', img_info); 
-            
 
             // setShowPopover({...showPopover, open: false});
             // showToast(`Loaded ${image["image_name"]}${image["image_ext"]}`, toastTime);
@@ -104,23 +126,21 @@ const CropMenu: React.FC<CropMenuProps> = (props: CropMenuProps) => {
                  onPreview={onPreview} onApply={onApply} onOther={onReset} OtherName="Reset">
                 <IonItem>
                     <IonLabel>Crop Image</IonLabel>
-                    {/* <IonToggle checked={toggleCropPreviewButton} onIonChange={handlerToggleCropPreviewMode}>
-                    </IonToggle> */}
                 </IonItem>
                 <IonItem>
-                    <IonRange name={'cropRangeX'} dualKnobs={true} value={cropX} min={0} max={props.imageShape.x} step={1} snaps={true} pin={true} ticks={false}
+                    <IonRange name={'cropRangeX'} ref={rangeRefX} dualKnobs={true} min={0} max={props.imageShape.x} step={1} snaps={true} pin={true} ticks={false}
                         onIonKnobMoveEnd={(ex) => { setCropX(ex.detail.value as any) }}>                        
                         <IonLabel slot="start"><h2>X</h2></IonLabel>
                     </IonRange>
                 </IonItem>
                 <IonItem>
-                    <IonRange name={'cropRangeY'} dualKnobs={true} value={cropY} min={0} max={props.imageShape.y} step={1} snaps={true} pin={true} ticks={false}
+                    <IonRange name={'cropRangeY'} ref={rangeRefY} dualKnobs={true} min={0} max={props.imageShape.y} step={1} snaps={true} pin={true} ticks={false}
                         onIonKnobMoveEnd={(ey) => {setCropY(ey.detail.value as any) }}>
                         <IonLabel slot="start"><h2>Y</h2></IonLabel>
                     </IonRange>
                 </IonItem>
                 <IonItem>
-                    <IonRange name={'cropRangeZ'} dualKnobs={true} value={cropZ} min={0} max={props.imageShape.z} step={1} snaps={true} pin={true} ticks={false}
+                    <IonRange name={'cropRangeZ'} ref={rangeRefZ} dualKnobs={true} min={0} max={props.imageShape.z} step={1} snaps={true} pin={true} ticks={false}
                         onIonKnobMoveEnd={(ez) => {setCropZ(ez.detail.value as any) }}>
                         <IonLabel slot="start"><h2>Z</h2></IonLabel>
                     </IonRange>
@@ -128,7 +148,7 @@ const CropMenu: React.FC<CropMenuProps> = (props: CropMenuProps) => {
                 <IonItem>
                     <IonLabel className='ion-text-wrap'>
                         Selected Range:
-                        <p>
+                        <p className='ion-center'>
                             ({cropX.lower}:{cropX.upper}, {cropY.lower}:{cropY.upper}, {cropZ.lower}:{cropZ.upper})
                         </p>
                     </IonLabel>
