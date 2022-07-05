9e4bc3a07f8f96550de7652c6fd05bbbb47f02c8
gabriel.macedo <gabriel.macedo@lnls.br>
2022-07-05 13:12:59 -0300

    Some changes before i merge with dev-ionic

--- src/components/main_menu/deep_learning_menu/batch_inference_comp/InferenceComp.tsx
+++ src/components/main_menu/deep_learning_menu/batch_inference_comp/InferenceComp.tsx
@@ -7,14 +7,43 @@ import {
     IonItem,
     IonLabel,
     IonList,
-    IonPopover
+    IonPopover, IonSelect, IonSelectOption
 } from "@ionic/react";
 import ErrorWindowComp from "../../file/ErrorWindowComp";
 import {useStorageState} from "react-storage-hooks";
 import {addOutline, trashOutline} from "ionicons/icons";
+import {dtypeList} from "../../file/FileLoadInterface";
+import {InitTables, TableInterface} from "../dataset_compo/DatasetInterfaces";
+
+interface Network {
+    key: number,
+    value: string,
+    label: string
+}
+
+const typeNetworks: Network[] = [
+    {
+        key: 0,
+        value: "u-net",
+        label: "U-Net"
+    },
+    {
+        key: 1,
+        value: "v-net",
+        label: "V-Net"
+    },
+    {
+        key: 2,
+        value: "bla-net",
+        label: "Bla-Net"
+    },
+]
 
 const InferenceComp: React.FC = () => {
     const [disableComp, setDisableComp] = useStorageState<boolean>(sessionStorage, "workspaceLoaded", true);
+    const [InputImagesTable, setInputImagesTable] = useStorageState<TableInterface[]>(sessionStorage, 'inputImagesTable', InitTables);
+    const [idTable, setIdTable] = useStorageState<number>(sessionStorage, "idTable", 0);
+
     const [showErrorWindow, setShowErrorWindow] = useState<boolean>(false);
     const [errorMsg, setErrorMsg] = useState<string>("");
 
@@ -38,28 +67,23 @@ const InferenceComp: React.FC = () => {
         <small>
             <IonContent scrollEvents={true}>
                 <IonAccordionGroup multiple={true}>
+                    {/*Network option*/}
                     <IonAccordion>
                         <IonItem slot={"header"}>
-                            <IonLabel><small>Bla</small></IonLabel>
+                            <IonLabel><small>Network</small></IonLabel>
                         </IonItem>
                         <IonList slot={"content"}>
-                            <div style={{display: "flex", justifyContent: "flex-end"}}>
-                                <IonButton
-                                    id={"data-menu"}
-                                    size={"default"}>
-                                    <IonIcon
-                                        icon={addOutline}
-                                        slot={"end"}/>
-                                    Add
-                                </IonButton>
-                                <IonButton
-                                    color={"danger"}
-                                    size={"default"}
-                                    slot={"end"}>
-                                    <IonIcon icon={trashOutline} slot={"end"}/>
-                                    Delete all
-                                </IonButton>
-                            </div>
+                            <IonItem>
+                                <IonLabel>Network type</IonLabel>
+                                <IonSelect interface={"popover"}>
+                                    {typeNetworks.map((type) => {
+                                        return (
+                                            <IonSelectOption key={type.key}
+                                                value={type.value}>{type.label}</IonSelectOption>
+                                        );
+                                    })}
+                                </IonSelect>
+                            </IonItem>
                         </IonList>
                     </IonAccordion>
                 </IonAccordionGroup>
