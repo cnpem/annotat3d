3f223f581c091811f656a94909da7b77205f2bd4
Allan Pinto <allan.pinto@lnls.br>
2023-05-26 14:00:20 -0300

    Improve code quality

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,51 @@
-import {
-    IonApp,
-    IonContent,
-    IonMenu,
-    IonRouterOutlet,
-    IonSpinner,
-    IonSplitPane,
-    setupIonicReact,
-} from '@ionic/react'
-import { IonReactRouter } from '@ionic/react-router'
-import Menu from './components/main_menu/Menu'
-import Page from './pages/Page'
+import { IonApp, IonContent, IonMenu, IonRouterOutlet, IonSpinner, IonSplitPane, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import Menu from './components/main_menu/Menu';
+import Page from './pages/Page';
 
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css'
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css'
-import '@ionic/react/css/structure.css'
-import '@ionic/react/css/typography.css'
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css'
-import '@ionic/react/css/float-elements.css'
-import '@ionic/react/css/text-alignment.css'
-import '@ionic/react/css/text-transformation.css'
-import '@ionic/react/css/flex-utils.css'
-import '@ionic/react/css/display.css'
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
 /* Theme variables */
-import './theme/variables.css'
-import ToolsMenu from './components/tools_menu/ToolsMenu'
-import {useEffect, useState} from 'react'
-import {sfetch} from './utils/simplerequest'
+import './theme/variables.css';
+import ToolsMenu from './components/tools_menu/ToolsMenu';
+import { useEffect, useState } from 'react';
+import { sfetch } from './utils/simplerequest';
 
-setupIonicReact()
+setupIonicReact();
+
+const LoadingComponent: React.FC<{ hidden?: boolean }> = ({ hidden }) => {
+    return (
+        <IonContent hidden={hidden}>
+            <div style={{ display: 'flex', height: '100vh', alignItems: 'center', justifyContent: 'center' }}>
+                <IonSpinner
+                    color="primary"
+                    name="bubbles"
+                    duration={1500}
+                    style={{ transform: 'scale(5)' }}
+                ></IonSpinner>
+            </div>
+        </IonContent>
+    );
+};
 
 /**
  * Main function that does all the functions callouts
  */
 const App: React.FC = () => {
-
     //a sanity check is sent to check if the backend
     //is up every TEST_INTERVAL milliseconds
     const TEST_INTERVAL = 1000;
@@ -49,24 +55,22 @@ const App: React.FC = () => {
     function loopBackendTest() {
         console.log('loop backend test');
         sfetch('POST', '/test')
-        .then( () => {
-            setLoadedBackend(true);
-        })
-        .catch(() => {
-            setLoadedBackend(false);
-            setTimeout(loopBackendTest, TEST_INTERVAL);
-        });
-
+            .then(() => {
+                setLoadedBackend(true);
+            })
+            .catch(() => {
+                setLoadedBackend(false);
+                setTimeout(loopBackendTest, TEST_INTERVAL);
+            });
     }
 
     useEffect(() => {
         loopBackendTest();
     });
 
-
     return (
         <IonApp>
-            <LoadingComponent hidden={loadedBackend}/>
+            <LoadingComponent hidden={loadedBackend} />
             <IonReactRouter>
                 <Menu />
                 <IonSplitPane hidden={!loadedBackend} contentId="main" when="(orientation: landscape)">
@@ -75,7 +79,8 @@ const App: React.FC = () => {
                         menuId="custom"
                         id="custom"
                         contentId="main"
-                        onIonWillClose={(e) => console.log(e)}>
+                        onIonWillClose={(e) => console.log(e)}
+                    >
                         <IonContent>
                             <ToolsMenu hideMenu={true} />
                         </IonContent>
@@ -87,23 +92,7 @@ const App: React.FC = () => {
                 </IonSplitPane>
             </IonReactRouter>
         </IonApp>
-    )
-}
-
-const LoadingComponent: React.FC<{hidden?: boolean}> = ({hidden}) => {
-
-    return (
-        <IonContent hidden={hidden}>
-            <div style={ { display: 'flex',
-                height: '100vh',
-                alignItems: 'center',
-                justifyContent: 'center'} } >
-                <IonSpinner color="primary" name='bubbles' duration={1500}
-                    style={ { transform: 'scale(5)'} }>
-                </IonSpinner>
-            </div>
-        </IonContent>
     );
-}
+};
 
-export default App
+export default App;
