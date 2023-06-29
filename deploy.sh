4e54fe5f2e474f4d577707782c892374e9300e59
Allan Pinto <allan.pinto@lnls.br>
2023-06-29 18:06:54 -0300

    Fix deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,8 +6,8 @@ GCC_APPS=$3
 
 echo "Deploying ${VERSION} version on ${GCC_APPS}"
 
-cp ${SIF_FILE} "${GCC_APPS}"/ssc-annotat3d/Annotat3DWeb_${VERSION}.sif
-cp scripts/request_port.py${GCC_APPS}/ssc-annotat3d/
+cp ${SIF_FILE} ${GCC_APPS}/ssc-annotat3d/Annotat3DWeb_${VERSION}.sif
+cp scripts/request_port.py ${GCC_APPS}/ssc-annotat3d/
 cp scripts/Annotat3DWeb_${VERSION} ${GCC_APPS}/ssc-annotat3d/
 
 if [ ${VERSION} == 'production' ]; then
