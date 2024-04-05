5b29d343f2da94b4ab78dffa82c8c645e8a0fcff
Egon Borges <egon.borges@lnls.br>
2024-04-05 16:34:52 -0300

    feat!(backend): remove deep functions
    removed all deep learning and deepsirius functions

--- backend/sscAnnotat3D/help/about.py
+++ backend/sscAnnotat3D/help/about.py
@@ -1,21 +1,21 @@
 import os
+
 import PyQt5.QtWidgets as QtWidgets
-from PyQt5 import uic
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import QDialog
+from PyQt5 import QtCore, uic
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QDialog
+
 from . import __ui_path__
+
 try:
-    from sscAnnotat3D import utils
     import sscPySpin
-    import sscDeepsirius
-    sscDeepsiriusVersion = sscDeepsirius.__version__
+    from sscAnnotat3D import utils
+
     sscPySpinVersion = sscPySpin.__version__
 except:
-    sscPySpinVersion = 'unknown'
-    sscDeepsiriusVersion = 'unknown'
-#move to a global constant later
-__version__ = '0.10.5'
+    sscPySpinVersion = "unknown"
+# move to a global constant later
+__version__ = "0.10.5"
 
 
 class about_help(QDialog):
@@ -25,14 +25,13 @@ class about_help(QDialog):
 
         utils.new_console(self)
 
-        uic.loadUi(os.path.join(__ui_path__, 'help/about.ui'), self)
+        uic.loadUi(os.path.join(__ui_path__, "help/about.ui"), self)
         self.setWindowTitle("About Annotat3D")
 
-        self.annotat3dVersion.setText('Annotat3D - {}'.format(__version__))
-        self.pyspinVersion.setText('PySpin - {}'.format(sscPySpinVersion))
-        self.deepsiriusVersion.setText('DeepSirius - {}'.format(sscDeepsiriusVersion))
+        self.annotat3dVersion.setText("Annotat3D - {}".format(__version__))
+        self.pyspinVersion.setText("PySpin - {}".format(sscPySpinVersion))
 
-        pixmap = QPixmap(os.path.join(__ui_path__, 'icons/logo-cnpem.png'))
+        pixmap = QPixmap(os.path.join(__ui_path__, "icons/logo-cnpem.png"))
 
         self.logo.setPixmap(pixmap)
 
