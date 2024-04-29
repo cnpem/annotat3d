7e38bb7dfffd1f4a3585635e4f2159f3569708ab
Allan Pinto <allan.pinto@lnls.br>
2024-04-29 11:07:52 -0300

    build: fix HPCCM and Singularity recipes

--- container/hpcbase-Annotat3DWeb.py
+++ container/hpcbase-Annotat3DWeb.py
@@ -3,32 +3,29 @@ Annotat3DWeb Base Image
 
 Contents:
     Ubuntu version 20.04
-    NVIDIA CUDA version 11.2.0 including cuBLAS version 11.3.1
-    NVIDIA cuDNN version 8.1.0
-    NVIDIA NCCL version 2.8.4 (optimized for NVLink)
+    NVIDIA CUDA version 11.6 including cuBLAS version 11.8.1.74
+    NVIDIA cuDNN version 8.3.2.44
+    NVIDIA NCCL version 2.11.4 (optimized for NVLink)
     APEX
-    TensorBoard version 1.15.5
-    Nsight Compute version 2020.3.0.18
-    Nsight Systems version 2020.4.3.7
-    TensorRT version7.2.2.3+cuda11.1.0.024
-    DALI version 0.29.0
+    RDMA Core version 36.0
+    TensorBoard version 2.8.0
+    PyTorch version 1.11.0
+    Nsight Compute version 2022.1.0
+    Nsight Systems version 2021.5.2.53
+    TensorRT version 8.2.3
+    DALI version 1.10.0
     MAGMA version 2.5.2
-    DLProf version 0.19.0
-    PyProf version r21.02
-    OpenMPI version 4.0.5
-    GNU compilers version 8
+    GNU compilers version 9
+    OpenMPI version 4.1.2
     Mellanox OFED version 5.1-2.5.8.0
-    HDF5 version 1.10.5
-    Anaconda version 23.1.0-1
     Node version v20.12.2
     Annotat3DWeb Dependencies
 """
 
 # pylint: disable=invalid-name, undefined-variable, used-before-assignment
 
-import argparse
-
 import hpccm
+import argparse
 
 
 def parse_args():
@@ -45,6 +42,13 @@ def parse_args():
         type=str,
         default="3.6",
         help="Singularity version (default=%(default)s).",
+    )
+    parser.add_argument(
+        "--distro",
+        type=str,
+        default="ubuntu20",
+        choices=['ubuntu20', 'ubuntu22'],
+        help="Linux distribution(default=%(default)s).",
     )
 
     return parser.parse_args()
@@ -65,14 +69,46 @@ def main():
     stage += hpccm.primitives.comment(__doc__, reformat=False)
 
     # Base image definition
-    stage += hpccm.primitives.baseimage(image="nvcr.io/nvidia/pytorch:21.02-py3", _distro="ubuntu20", _arch="x86_64")
+    stage += hpccm.primitives.baseimage(image="nvcr.io/nvidia/pytorch:22.02-py3", _distro=args.distro)
 
-    stage += hpccm.primitives.label(
-        metadata={
-            'br.lnls.gcd.mantainer': 'GCD Team <gcd.lnls.br>',
-            'br.lnls.gcd.release': 'production',
-            'br.lnls.gcd.version': '21.02-py3',
-    })
+    # stage += hpccm.primitives.label(
+    #     metadata={
+    #         "br.lnls.gcd.mantainer": "GCD Group <gcd.lnls.br>",
+    #         "br.lnls.gcd.version": "22.02-py3",
+    # })
+
+    # variables
+    stage += hpccm.primitives.comment("GCD package repository ID")
+    stage += hpccm.primitives.environment(
+        variables={
+            "GCD_PKG_REPO": "3702",
+        }
+    )
+
+    stage += hpccm.primitives.environment(
+        variables={
+            "CUDA_HOME": "/usr/local/cuda",
+            "CUDA_TOOLKIT_PATH": "$CUDA_HOME",
+        }
+    )
+
+    stage += hpccm.primitives.environment(
+        variables={
+            "CPATH": "/usr/include/hdf5/serial/:$CPATH",
+            "HDF5_INCLUDE_PATH": "/usr/include/hdf5/serial/:$HDF5_INCLUDE_PATH",
+            "LD_LIBRARY_PATH": "/usr/lib/x86_64-linux-gnu:$CUDA_HOME/lib64:$CUDA_HOME/lib64/stubs:$LD_LIBRARY_PATH",
+            "LIBRARY_PATH": "/usr/lib/x86_64-linux-gnu:$CUDA_HOME/lib64:$CUDA_HOME/lib64/stubs:$LIBRARY_PATH",
+            "NODE_PATH": "$NVM_DIR/v$NODE_VERSION/lib/node_modules",
+            "PATH": "$CUDA_HOME/bin:$CUDA_HOME/nvvm/bin:$NVM_DIR/versions/node/v$NODE_VERSION/bin:$PATH",
+        }
+    )
+
+    stage += hpccm.primitives.environment(
+        variables={
+            "NODE_VERSION": "20.12.2",
+            "NVM_DIR": "/usr/local/nvm",
+        }
+    )
 
     # Install development tools
     ospackages = [
@@ -98,7 +134,10 @@ def main():
         "libgl1",
         "libgl1-mesa-dev",
         "libglib2.0-0",
+        "libhdf5-*",
+        "libnetcdf-dev",
         "libnss3",
+        "libopenmpi-dev",
         "libssl-dev",
         "libtool",
         "libxss1",
@@ -116,73 +155,72 @@ def main():
         "wget",
         "xz-utils",
         "zip",
+        "zlib1g-dev",
     ]
     stage += hpccm.building_blocks.apt_get(ospackages=ospackages)
 
-    # variables
-    stage += hpccm.primitives.environment(variables={"CUDA_HOME": "/usr/local/cuda", "CUDA_TOOLKIT_PATH": "$CUDA_HOME"})
-
-    stage += hpccm.primitives.environment(
-        variables={
-            "CC": "gcc",
-            "CXX": "g++",
-        }
-    )
-
-    stage += hpccm.primitives.environment(
-        variables={
-            "NODE_VERSION": "20.12.2",
-            "NVM_DIR": "/usr/local/nvm",
-        }
-    )
-
-    stage += hpccm.primitives.environment(
-        variables={
-            "NODE_PATH": "$NVM_DIR/v$NODE_VERSION/lib/node_modules",
-            "PATH": "/usr/local/cuda/bin:/usr/local/cuda/nvvm/bin:$NVM_DIR/versions/node/v$NODE_VERSION/bin:$PATH",
-            "LD_LIBRARY_PATH": "/usr/lib/x86_64-linux-gnu:$CUDA_HOME/lib64:$CUDA_HOME/lib64/stubs:$LD_LIBRARY_PATH",
-            "LIBRARY_PATH": "/usr/lib/x86_64-linux-gnu:$CUDA_HOME/lib64:$CUDA_HOME/lib64/stubs:$LIBRARY_PATH",
-        }
-    )
-
-    # GNU compilers
-    compiler = hpccm.building_blocks.gnu(version="8")
-    stage += compiler
-
     # Mellanox OFED
-    stage += hpccm.building_blocks.mlnx_ofed(version="5.1-2.5.8.0", _distro="ubuntu20")
-
-    # HDF5
-    stage += hpccm.building_blocks.hdf5(version="1.10.5", toolchain=compiler.toolchain)
-
-    # Python
-    stage += hpccm.primitives.shell(commands=["rm -rf /opt/conda"], chdir=False)
-    stage += hpccm.building_blocks.conda(version="23.1.0-1", python_subversion="py39", prefix="/opt/conda", eula=True)
+    stage += hpccm.building_blocks.mlnx_ofed(version="5.1-2.5.8.0", _distro=args.distro)
 
     # Configuring pip
     stage += hpccm.primitives.shell(
         commands=[
-            "python3 -m pip config --user set global.extra-index-url http://gcc.lnls.br:3128",
-            "python3 -m pip config --user set global.trusted-host gcc.lnls.br",
+            "python3 -m pip config set global.extra-index-url https://gitlab.cnpem.br/api/v4/projects/3702/packages/pypi/simple",
+            "python3 -m pip config set global.trusted-host gitlab.cnpem.br",
+            "conda install -c conda-forge -y mpi4py==3.1.4",
+            "conda clean -afy",
         ],
         chdir=False,
     )
+
 
     # Install python build dependencies
     stage += hpccm.building_blocks.pip(
         ospackages=[],
         packages=[
-            "pip==22.0.4",
-            "setuptools==69.0.3",
-            "wheel",
-            "cmake==3.17.3",
-            "cython==0.29.30",
+            "build",
             "cmake-setuptools",
-            "SharedArray==3.2.0",
-            "nibabel",
-            "blinker",
+            "cmake>=3.18.0",
+            "cmake_setup",
+            "cython==0.29.30",
+            "pip>=22.0.4",
+            "setuptools>=69.0.3",
+            "wheel",
         ],
         pip="pip3",
+    )
+
+    stage += hpccm.building_blocks.pip(
+        ospackages=[],
+        packages=[
+            "blinker",
+            "cbf",
+            "nibabel",
+            "SharedArray==3.2.0",
+            "sysv_ipc",
+        ],
+        pip="pip3",
+    )
+
+    stage += hpccm.primitives.shell(
+        commands=[
+            "conda install -c conda-forge -y mpi4py==3.1.4",
+            "conda clean -afy",
+        ],
+        chdir=False,
+    )
+
+    # Install sscIO
+    stage += hpccm.primitives.copy(src="externals/sscIO", dest="/opt/sscIO")
+
+    stage += hpccm.primitives.shell(
+        commands=[
+            "cd /opt/sscIO/python",
+            "python3 -m pip install --upgrade pip",
+            "python3 -m pip install -r requirements.txt",
+            "python3 setup.py bdist_wheel",
+        ],
+        chdir=False,
     )
 
     # Install requirements
@@ -193,8 +231,7 @@ def main():
     stage += hpccm.primitives.shell(
         commands=[
             ". $NVM_DIR/nvm.sh",
-            "nvm install v$NODE_VERSION --reinstall-packages-from=v15",
-            "nvm uninstall v15",
+            "nvm install v$NODE_VERSION",
             "nvm alias default v$NODE_VERSION",
             "nvm use default",
         ],
@@ -202,6 +239,9 @@ def main():
     )
 
     stage += hpccm.primitives.shell(commands=["npm install -g ionic yarn serve"], chdir=False)
+
+    # setup NVM environment
+    stage += hpccm.primitives.shell(commands=["echo 'source $NVM_DIR/nvm.sh' >> /etc/bash.bashrc"], chdir=False)
 
     # Output container specification
     print(stage)
