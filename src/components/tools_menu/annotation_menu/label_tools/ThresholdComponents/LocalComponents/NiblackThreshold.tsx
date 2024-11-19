d8f9031afc45e764df13db18ca9cad991ecf2ea4
ricardo.grangeiro <ricardo.grangeiro@lnls.br>
2024-11-19 18:04:44 -0300

    feat: niblack interface

--- src/components/tools_menu/annotation_menu/label_tools/ThresholdComponents/LocalComponents/NiblackThreshold.tsx
+++ src/components/tools_menu/annotation_menu/label_tools/ThresholdComponents/LocalComponents/NiblackThreshold.tsx
@@ -1,11 +1,108 @@
 import React from 'react';
-import { IonItem, IonLabel } from '@ionic/react';
+import {
+    IonItem,
+    IonLabel,
+    IonInput,
+    IonButton,
+    IonIcon,
+    IonPopover,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardContent,
+    IonSelect,
+    IonSelectOption,
+    IonRadio,
+    IonRadioGroup,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonNote,
+} from '@ionic/react';
+import { informationCircleOutline } from 'ionicons/icons';
 
-const NiblackThreshold: React.FC = () => (
-    <IonItem>
-        <IonLabel>Niblack Threshold Component</IonLabel>
-        {/* Add your Niblack Threshold component implementation here */}
-    </IonItem>
-);
+// KernelInputWithInfo Component
+interface NiblackThresholdProps {
+    Kernel: number;
+    setKernelSize: (value: number) => void;
+    showToast: (message: string, duration: number) => void;
+    timeToast: number;
+    Weight: number;
+    setWeight: (value: number) => void;
+}
+
+const NiblackThreshold: React.FC<NiblackThresholdProps> = ({
+    Kernel,
+    setKernelSize,
+    showToast,
+    timeToast,
+    Weight,
+    setWeight,
+}) => {
+    return (
+        <>
+            <IonItem>
+                <IonLabel>Kernel: </IonLabel>
+                <IonInput
+                    value={Kernel}
+                    type="number"
+                    step="1"
+                    min={3}
+                    onIonChange={(e) =>
+                        typeof +e.detail.value! === 'number'
+                            ? setKernelSize(+e.detail.value!)
+                            : void showToast('Please insert a valid number!', timeToast)
+                    }
+                ></IonInput>
+                <IonButton id="showKernelSizeInfo" slot="end" size="small" fill="clear">
+                    <IonIcon icon={informationCircleOutline} />
+                </IonButton>
+                <IonPopover trigger="showKernelSizeInfo" reference="event">
+                    <IonContent>
+                        <IonCard>
+                            <IonCardHeader>
+                                <div style={{ fontWeight: 600, fontSize: 14 }}>Kernel Size</div>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                Specifies the size of the kernel, which defines the area/volume around each pixel/voxel
+                                that the filter considers when processing the image. A larger kernel means more
+                                neighboring elements are included in the calculation.
+                            </IonCardContent>
+                        </IonCard>
+                    </IonContent>
+                </IonPopover>
+            </IonItem>
+            <IonItem>
+                <IonLabel>Weight: </IonLabel>
+                <IonInput
+                    value={Weight}
+                    type="number"
+                    step="0.1"
+                    min={0}
+                    onIonChange={(e) =>
+                        typeof +e.detail.value! === 'number'
+                            ? setWeight(+e.detail.value!)
+                            : void showToast('Please insert a valid number!', timeToast)
+                    }
+                ></IonInput>
+                <IonButton id="showWeightInfo" slot="end" size="small" fill="clear">
+                    <IonIcon icon={informationCircleOutline} />
+                </IonButton>
+                <IonPopover trigger="showWeightInfo" reference="event">
+                    <IonContent>
+                        <IonCard>
+                            <IonCardHeader>
+                                <div style={{ fontWeight: 600, fontSize: 14 }}>Weight Value</div>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                Defines the weight of the standard deviation during the threshold.
+                            </IonCardContent>
+                        </IonCard>
+                    </IonContent>
+                </IonPopover>
+            </IonItem>
+        </>
+    );
+};
 
 export default NiblackThreshold;
