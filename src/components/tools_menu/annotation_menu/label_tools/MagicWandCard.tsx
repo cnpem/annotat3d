06b1ce4a4b63ea93aa398388ed2d40ea228d6dd3
Egon Borges <egon.borges@lnls.br>
2025-02-05 16:26:13 -0300

    fix: histogram float values for magicwand

--- src/components/tools_menu/annotation_menu/label_tools/MagicWandCard.tsx
+++ src/components/tools_menu/annotation_menu/label_tools/MagicWandCard.tsx
@@ -18,6 +18,7 @@ import { useStorageState } from 'react-storage-hooks';
 import { sfetch } from '../../../../utils/simplerequest';
 import enter from '../../../../public/icon-park-solid--enter-key.svg';
 import LoadingComponent from '../../utils/LoadingComponent';
+import { HistogramInfoPayload } from '../../../main_menu/file/utils/HistogramInfoInterface';
 
 interface MagicWandCardProps {
     isVisible: boolean;
@@ -91,21 +92,103 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
 
     const [verticalLinePosition, setVerticaline] = useStorageState(sessionStorage, 'verticalLinePosition', 0);
     //get saved state from the same key, 'histogramData' in vis_menu and contrast min and max
-    const [histogramData, setHistogramData] = useStorageState(sessionStorage, 'histogramData', baseHistogram);
-    const [contrastRangeRefMaxValue, setContrastRangeRefMaxValue] = useStorageState(
-        sessionStorage,
-        'contrastRangeRefMaxValue',
-        100
-    );
-    const [contrastRangeRefMinValue, setContrastRangeRefMinValue] = useStorageState(
-        sessionStorage,
-        'contrastRangeRefMinValue',
-        0
-    );
-    const [contrast, setContrast] = useState({
-        lower: contrastRangeRefMinValue,
-        upper: contrastRangeRefMaxValue,
+    const [histogramData, setHistogramData] = useState(baseHistogram);
+
+    // This is the bin index number that the user can change
+    const [bin, setBin] = useState({
+        lower: 0,
+        upper: 254,
     });
+
+    const [pinFormatter, setPinFormatter] = useState<(binNumber: number) => number>(
+        () => (binNumber: number) => binNumber
+    );
+
+    const [contrast, setContrast] = useState({
+        lower: 0,
+        upper: 100,
+    });
+    const [histogramMinMaxValue, sethistogramMinMaxValue] = useState({
+        max: 0,
+        min: 100,
+    });
+
+    // Function to update bin and adjust contrast accordingly
+    const updateBin = (newBin: { lower: number; upper: number }) => {
+        setBin(newBin);
+
+        // Example: Adjust contrast based on bin change (modify logic as needed)
+        setContrast((prevContrast) => ({
+            lower: histogramData.labels[newBin.lower],
+            upper: histogramData.labels[newBin.upper],
+        }));
+    };
+    function getClosestIndex(arr: number[], target: number): number {
+        return arr.reduce(
+            (closestIndex, curr, index) =>
+                Math.abs(curr - target) < Math.abs(arr[closestIndex] - target) ? index : closestIndex,
+            0
+        );
+    }
+    // Function to update contrast by enter
+    const updateContrast = (newContrast: { lower: number; upper: number }) => {
+        setContrast(newContrast);
+        // Example: Adjust contrast based on bin change (modify logic as needed)
+        setBin((prevBin) => ({
+            lower: getClosestIndex(histogramData.labels, newContrast.lower),
+            upper: getClosestIndex(histogramData.labels, newContrast.upper),
+        }));
+    };
+    const [loadingMsg, setLoadingMsg] = useState<string>('');
+    const [showLoadingComp, setShowLoadingComp] = useState<boolean>(false);
+    //Change this to sfetch to load histogram from backend
+    const fetchHistogramData = () => {
+        const sliceValue = parseInt(sessionStorage.getItem('sliceValue') || '0', 10); // For numbers
+        const sliceName = JSON.parse(sessionStorage.getItem('sliceName') || '"XY"'); // For strings with JSON-like quotes
+        // Fetch or compute 2D histogram data
+        const params = {
+            axis: sliceName,
+            slice: sliceValue,
+        };
+        setLoadingMsg('Loading 2D histogram');
+        setShowLoadingComp(true);
+        sfetch('POST', '/get_image_histogram/image', JSON.stringify(params), 'json')
+            .then((loadedHistogram: HistogramInfoPayload) => {
+                const updatedHistogram = {
+                    labels: loadedHistogram.bins,
+                    datasets: [
+                        {
+                            data: loadedHistogram.data,
+                            borderColor: ['rgba(0, 0, 0, 0.5)', 'rgba(0, 0, 0, 0.5)'],
+                            backgroundColor: ['rgba(0, 0, 0, 0.5)', 'rgba(0, 0, 0, 0.5)'],
+                            normalized: true,
+                        },
+                    ],
+                };
+                // Define pinFormatter based on data type
+                const newPinFormatter = (binNumber: number): number => {
+                    console.log(
+                        'Loaded histogram includes int?',
+                        loadedHistogram.dataType.includes('int'),
+                        loadedHistogram.dataType
+                    );
+                    return loadedHistogram.dataType.includes('int')
+                        ? Math.round(loadedHistogram.bins[binNumber])
+                        : Math.round(loadedHistogram.bins[binNumber] * 100) / 100;
+                };
+                setPinFormatter(() => newPinFormatter); // Update state with new function
+
+                setHistogramData(updatedHistogram);
+                sethistogramMinMaxValue({ min: loadedHistogram.minValue, max: loadedHistogram.maxValue });
+                setShowLoadingComp(false);
+            })
+            .catch((error) => {
+                console.log('Error while acquiring histogram');
+                console.log(error.error_msg);
+                setShowLoadingComp(false);
+            });
+    };
+
     const [bluRadius, setBlur] = useState(0);
     //I'm using event bus to get update this, but it should use useStorageState when canvas is updated to new react logic (with react hooks)
     const [dataWand, setdataWand] = useState({
@@ -115,8 +198,6 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
         x_coord: 0,
         y_coord: 0,
     });
-    const [loadingMsg, setLoadingMsg] = useState<string>('');
-    const [showLoadingComp, setShowLoadingComp] = useState<boolean>(false);
 
     const fetchDataWand = (newClick: boolean) => {
         const updatedDataWand = {
@@ -125,8 +206,8 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
             upper_min: contrast.lower,
             blur_radius: bluRadius,
             new_click: newClick,
-            max_contrast: contrastRangeRefMaxValue,
-            min_contrast: contrastRangeRefMinValue,
+            max_contrast: histogramMinMaxValue.max,
+            min_contrast: histogramMinMaxValue.min,
         };
         console.log('Applying MagicWand');
         setLoadingMsg('Applying wand and updating annotation');
@@ -141,8 +222,8 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
                 if (newClick) {
                     skipEffectRef.current = true;
                     // Tolerance is set to 8% of the width in histogram
-                    const Tolerance = (8 * (contrastRangeRefMaxValue - contrastRangeRefMinValue)) / 100;
-                    setContrast({ lower: StartValue - Tolerance, upper: StartValue + Tolerance });
+                    const Tolerance = (8 * (histogramMinMaxValue.max - histogramMinMaxValue.min)) / 100;
+                    updateContrast({ lower: StartValue - Tolerance, upper: StartValue + Tolerance });
                 }
             })
             .catch((error) => {
@@ -155,6 +236,7 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
         //only execute if the button is pressed
         if (isVisible) {
             dispatch('ChangeStateBrush', 'magic_wand');
+            fetchHistogramData();
         } else {
             dispatch('ChangeStateBrush', 'draw_brush');
         }
@@ -163,6 +245,13 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
     // Updates the datawand, and use a hooke to update the state
     useEventBus('magicwand', (data) => {
         setdataWand(data);
+    });
+
+    // Reload when slice is changed
+    useEventBus('sliceChanged', (payload) => {
+        if (isVisible) {
+            fetchHistogramData();
+        }
     });
     // Its not possible to use eventbus, as it doesn't rerender, so, we need to use a new react hook for updates in isVisible
     useEffect(() => {
@@ -208,15 +297,15 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
                             <IonItem>
                                 <IonInput
                                     inputMode="numeric"
-                                    max={contrastRangeRefMaxValue}
-                                    min={contrastRangeRefMinValue}
+                                    max={histogramMinMaxValue.max}
+                                    min={histogramMinMaxValue.min}
                                     placeholder="Lower"
                                     onKeyUp={(e: React.KeyboardEvent) => {
                                         if (e.key === 'Enter') {
                                             const target = e.target as HTMLInputElement;
                                             const inputValue = parseFloat(target.value);
                                             if (target.value && inputValue < verticalLinePosition) {
-                                                setContrast({ upper: contrast.upper, lower: inputValue });
+                                                updateContrast({ upper: contrast.upper, lower: inputValue });
                                             }
                                         }
                                     }}
@@ -228,15 +317,15 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
                             <IonItem>
                                 <IonInput
                                     inputMode="numeric"
-                                    max={contrastRangeRefMaxValue}
-                                    min={contrastRangeRefMinValue}
+                                    max={histogramMinMaxValue.max}
+                                    min={histogramMinMaxValue.min}
                                     placeholder="Upper"
                                     onKeyUp={(e: React.KeyboardEvent) => {
                                         if (e.key === 'Enter') {
                                             const target = e.target as HTMLInputElement;
                                             const inputValue = parseFloat(target.value);
                                             if (target.value && inputValue > verticalLinePosition) {
-                                                setContrast({ lower: contrast.lower, upper: inputValue });
+                                                updateContrast({ lower: contrast.lower, upper: inputValue });
                                             }
                                         }
                                     }}
@@ -256,14 +345,15 @@ const MagicWandCard: React.FC<MagicWandCardProps> = ({ isVisible }) => {
                 <IonRange
                     className="custom-range"
                     step={1}
-                    max={contrastRangeRefMaxValue}
-                    min={contrastRangeRefMinValue}
-                    value={{ lower: contrast.lower, upper: contrast.upper }}
+                    max={histogramData.labels.length - 1}
+                    min={0}
+                    value={{ lower: bin.lower, upper: bin.upper }}
+                    pinFormatter={pinFormatter}
                     pin={true}
                     dualKnobs={true}
                     onIonKnobMoveEnd={(e: CustomEvent) => {
                         if (e.detail.value) {
-                            setContrast(e.detail.value);
+                            updateBin(e.detail.value);
                         }
                     }}
                 ></IonRange>
