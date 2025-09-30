8962781626757895df408d5ed46bc96d6ea617c2
Allan Pinto <allan.pinto@lnls.br>
2025-09-30 11:39:50 -0300

    refactor(container): improve Docker/Singularity/HPCCM recipes

--- container/build_docker.sh
+++ container/build_docker.sh
@@ -2,8 +2,7 @@
 
 # WARNING: This script must be executed from project root directory
 
-# -- Create a Dockerfile from the HPCCM recipe
-python3 container/hpccm-cuda-gcc-openmpi-hdf-conda.py --format docker > container/Dockerfile
-
-# -- Build a Docker Image
-docker build -t gitregistry.cnpem.br/gcd/data-science/segmentation/annotat3dweb -f container/Dockerfile .
+rm -f container/Dockerfile
+python3 container/hpccm-annotat3d.py --format docker --stage base > container/Dockerfile
+python3 container/hpccm-annotat3d.py --format docker --stage production >> container/Dockerfile
+docker build -t annotat3d-prod:latest -f container/Dockerfile .
